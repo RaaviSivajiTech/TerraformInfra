a110378c402d927128f49b1f51fa4f38ae5b6630
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-19 15:59:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,7 @@ provider "aws" {
 resource "aws_instance" "aws-jenkins-ec2" {
   ami = "ami-0b8b44ec9a8f90422"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  tags = {
+   tags = {
     Name = "Jenkins_Server"
   }
 
