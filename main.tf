2aa52aa3adc935a6a30230e49290ca5e56b6497b
RaaviSivajiTech <155118588+RaaviSivajiTech@users.noreply.github.com>
2024-03-19 15:29:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">= 1.2.0"
 }
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   access_key = "AKIARNKVYT4YJ7B5G3HS"
   secret_key = "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"
 }
